0a6b64c92b291bb6bb0f98623cd71fa9f30d2a78
S <6alyamov@gmail.com>
2013-07-18 00:33:07 +0400

    StateService refactoring

--- Alicargo.Database/Tables/AvailableState.sql
+++ Alicargo.Database/Tables/AvailableState.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[AvailableState] (
+    CONSTRAINT [PK_dbo.AvailableState] PRIMARY KEY CLUSTERED ([RoleId] ASC, [StateId] ASC),
     [RoleId]  INT    NOT NULL,
     [StateId] BIGINT NOT NULL,
-    CONSTRAINT [PK_dbo.AvailableState] PRIMARY KEY CLUSTERED ([RoleId] ASC, [StateId] ASC),
     CONSTRAINT [FK_dbo.AvailableState_dbo.State_StateId] FOREIGN KEY ([StateId]) REFERENCES [dbo].[State] ([Id]) ON DELETE CASCADE
 );
 
