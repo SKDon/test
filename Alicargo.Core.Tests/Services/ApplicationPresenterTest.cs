800f508ed15cc4683c32a6dc04c2e8617411ac5d
S <6alyamov@gmail.com>
2013-07-17 23:11:22 +0400

    tests refactoring

---
+++ Alicargo.Core.Tests/Services/ApplicationPresenterTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Alicargo.Core.Contracts;
+using Alicargo.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ploeh.AutoFixture;
+
+namespace Alicargo.Tests.Services
+{
+	[TestClass]
+	public class ApplicationPresenterTest
+	{
+		private TestContext _context;
+		private ApplicationPresenter _service;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_context = new TestContext();
+
+			_service = _context.Create<ApplicationPresenter>();
+		}
+
+		[TestMethod, Ignore]
+		public void Test_GetAvailableStates_CargoIsCustomsClearedStateId()
+		{
+			var cargoIsCustomsClearedStateId = _context.Create<long>();
+			var applicationData = _context.Build<ApplicationData>()
+				.With(x => x.StateId, cargoIsCustomsClearedStateId)
+				.Create();
+			var cargoReceivedStateId = _context.Create<long>();
+
+			var cargoOnTransitStateId = _context.Create<long>();
+			var dictionary = _context.Create<Dictionary<long, string>>();
+
+			_context.ApplicationRepository.Setup(x => x.Get(applicationData.Id)).Returns(applicationData);
+			_context.StateConfig.Setup(x => x.CargoIsCustomsClearedStateId).Returns(cargoIsCustomsClearedStateId);
+			_context.StateConfig.Setup(x => x.CargoOnTransitStateId).Returns(cargoOnTransitStateId);
+			_context.StateConfig.Setup(x => x.CargoReceivedStateId).Returns(cargoReceivedStateId);
+			_context.StateService.Setup(x => x.GetLocalizedDictionary(new[] { cargoOnTransitStateId, cargoReceivedStateId, cargoIsCustomsClearedStateId }))
+				.Returns(dictionary);
+
+			var stateModels = _service.GetAvailableStates(applicationData.Id);
+
+			foreach (var model in stateModels)
+			{
+				Assert.IsTrue(dictionary.ContainsKey(model.StateId));
+			}
+
+			_context.ApplicationRepository.Verify(x => x.Get(applicationData.Id), Times.Once());
+			_context.StateConfig.Verify(x => x.CargoIsCustomsClearedStateId);
+			_context.StateConfig.Verify(x => x.CargoOnTransitStateId, Times.Once());
+			_context.StateConfig.Verify(x => x.CargoReceivedStateId, Times.Once());
+			_context.StateService.Verify(x => x.GetLocalizedDictionary(new[] { cargoOnTransitStateId, cargoReceivedStateId, cargoIsCustomsClearedStateId }),
+				Times.Once());
+		}
+	}
+}
---
+++ Alicargo.Core.Tests/Services/StateServiceTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Linq;
+using Alicargo.Core.Enums;
+using Alicargo.Core.Exceptions;
+using Alicargo.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Alicargo.Tests.Services
+{
+	[TestClass]
+	public class StateServiceTests
+	{
+		private TestContext _context;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_context = new TestContext();
+		}
+
+		[TestMethod]
+		public void Test_HasPermissionToState_True()
+		{
+			const int stateId = 1;
+
+			_context.StateRepository.Setup(x => x.GetAvailableRoles(stateId)).Returns(new[] { RoleType.Brocker, RoleType.Client });
+			_context.IdentityService.Setup(x => x.IsInRole(RoleType.Brocker)).Returns(true);
+			_context.IdentityService.Setup(x => x.IsInRole(RoleType.Client)).Returns(false);
+
+			var stateService = _context.Create<StateService>();
+
+			Assert.IsTrue(stateService.HasPermissionToSetState(stateId));
+
+			_context.IdentityService.Verify(x => x.IsInRole(RoleType.Brocker), Times.Once());
+			_context.StateRepository.Verify(x => x.GetAvailableRoles(stateId), Times.Once());
+		}
+
+		[TestMethod]
+		public void Test_HasPermissionToState_False()
+		{
+			const int stateId = 1;
+
+			_context.StateRepository.Setup(x => x.GetAvailableRoles(stateId)).Returns(new[] { RoleType.Brocker, RoleType.Client });
+			_context.IdentityService.Setup(x => x.IsInRole(RoleType.Brocker)).Returns(false);
+			_context.IdentityService.Setup(x => x.IsInRole(RoleType.Client)).Returns(false);
+
+			var stateService = _context.Create<StateService>();
+
+			Assert.IsFalse(stateService.HasPermissionToSetState(stateId));
+
+			_context.IdentityService.Verify(x => x.IsInRole(RoleType.Brocker), Times.Once());
+			_context.IdentityService.Verify(x => x.IsInRole(RoleType.Client), Times.Once());
+			_context.StateRepository.Verify(x => x.GetAvailableRoles(stateId), Times.Once());
+		}
+
+		[TestMethod]
+		public void Test_GetAvailableStates()
+		{
+			var roles = Enum.GetValues(typeof(RoleType)).Cast<RoleType>().Except(new[] { RoleType.Brocker }).ToArray();
+			var states = _context.CreateMany<long>().ToArray();
+
+			foreach (var roleType in roles)
+			{
+				var type = roleType;
+				_context.IdentityService.Setup(x => x.IsInRole(type)).Returns(false);
+			}
+
+			foreach (var roleType in roles)
+			{
+				var type = roleType;
+				_context.IdentityService.Setup(x => x.IsInRole(type)).Returns(true);
+				_context.StateRepository.Setup(x => x.GetAvailableStates(type)).Returns(states);
+
+				var stateService = _context.Create<StateService>();
+				var availableStates = stateService.GetAvailableStates();
+
+				_context.AreEquals(states, availableStates);
+				_context.IdentityService.Verify(x => x.IsInRole(type));
+				_context.StateRepository.Verify(x => x.GetAvailableStates(type));
+
+				_context.IdentityService.Setup(x => x.IsInRole(type)).Returns(false);
+				_context.StateRepository.Setup(x => x.GetAvailableStates(type)).Throws<InvalidOperationException>();
+			}
+		}
+
+		[TestMethod, ExpectedException(typeof(InvalidLogicException))]
+		public void Test_GetAvailableStates_Brocker()
+		{
+			var roles = Enum.GetValues(typeof(RoleType)).Cast<RoleType>().Except(new[] { RoleType.Brocker }).ToArray();
+			foreach (var roleType in roles)
+			{
+				var type = roleType;
+				_context.IdentityService.Setup(x => x.IsInRole(type)).Returns(false);
+			}
+
+			var stateService = _context.Create<StateService>();
+			stateService.GetAvailableStates();
+		}
+
+		[TestMethod]
+		public void Test_GetVisibleStates()
+		{
+			var roles = Enum.GetValues(typeof(RoleType)).Cast<RoleType>().Except(new[] { RoleType.Brocker }).ToArray();
+			var states = _context.CreateMany<long>().ToArray();
+
+			foreach (var roleType in roles)
+			{
+				var type = roleType;
+				_context.IdentityService.Setup(x => x.IsInRole(type)).Returns(false);
+			}
+
+			foreach (var roleType in roles)
+			{
+				var type = roleType;
+				_context.IdentityService.Setup(x => x.IsInRole(type)).Returns(true);
+				_context.StateRepository.Setup(x => x.GetVisibleStates(type)).Returns(states);
+
+				var stateService = _context.Create<StateService>();
+				var availableStates = stateService.GetVisibleStates();
+
+				_context.AreEquals(states, availableStates);
+				_context.IdentityService.Verify(x => x.IsInRole(type));
+				_context.StateRepository.Verify(x => x.GetVisibleStates(type));
+
+				_context.IdentityService.Setup(x => x.IsInRole(type)).Returns(false);
+				_context.StateRepository.Setup(x => x.GetVisibleStates(type)).Throws<InvalidOperationException>();
+			}
+		}
+
+		[TestMethod, ExpectedException(typeof(InvalidLogicException))]
+		public void Test_GetVisibleStates_Brocker()
+		{
+			var roles = Enum.GetValues(typeof(RoleType)).Cast<RoleType>().Except(new[] { RoleType.Brocker }).ToArray();
+			foreach (var roleType in roles)
+			{
+				var type = roleType;
+				_context.IdentityService.Setup(x => x.IsInRole(type)).Returns(false);
+			}
+
+			var stateService = _context.Create<StateService>();
+			stateService.GetVisibleStates();
+		}
+	}
+}
