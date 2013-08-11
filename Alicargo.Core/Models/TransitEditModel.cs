2f0a09509aa9e1e950803b3c4d1b8f4484ee00fa
S <6alyamov@gmail.com>
2013-08-11 18:03:12 +0400

    minor

---
+++ Alicargo.Core/Models/TransitEditModel.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Alicargo.Contracts.Contracts;
+using Alicargo.Core.Enums;
+using Alicargo.Core.Localization;
+using Resources;
+
+namespace Alicargo.Core.Models
+{
+	public sealed class TransitEditModel
+	{
+		[Required, DisplayNameLocalized(typeof(Entities), "MethodOfTransit")]
+		public MethodOfTransit MethodOfTransit { get; set; }
+
+		[Required, DisplayNameLocalized(typeof(Entities), "DeliveryType")]
+		public DeliveryType DeliveryType { get; set; }
+
+		[Required, DisplayNameLocalized(typeof(Entities), "City")]
+		public string City { get; set; }
+
+		[Required, DisplayNameLocalized(typeof(Entities), "Address")]
+		public string Address { get; set; }
+
+		[Required, DisplayNameLocalized(typeof(Entities), "RecipientName")]
+		public string RecipientName { get; set; }
+
+		[Required, DisplayNameLocalized(typeof(Entities), "Phone")]
+		public string Phone { get; set; }
+
+		[DisplayNameLocalized(typeof(Entities), "WarehouseWorkingTime")]
+		public string WarehouseWorkingTime { get; set; }
+
+		public static TransitData GetData(TransitEditModel model, long carrierId)
+		{
+			return new TransitData
+			{
+				Address = model.Address,
+				CarrierId = carrierId,
+				WarehouseWorkingTime = model.WarehouseWorkingTime,
+				City = model.City,
+				DeliveryTypeId = (int)model.DeliveryType,
+				MethodOfTransitId = (int)model.MethodOfTransit,
+				Phone = model.Phone,
+				RecipientName = model.RecipientName
+			};
+		}
+
+		public static TransitEditModel GetModel(TransitData data)
+		{
+			return new TransitEditModel
+			{
+				Address = data.Address,
+				WarehouseWorkingTime = data.WarehouseWorkingTime,
+				City = data.City,
+				DeliveryType = (DeliveryType)data.DeliveryTypeId,
+				MethodOfTransit = (MethodOfTransit)data.MethodOfTransitId,
+				Phone = data.Phone,
+				RecipientName = data.RecipientName				
+			};
+		}
+	}
+}
