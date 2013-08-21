09f59f570353dfb4e97b359ec82b906819a42ac4
S <6alyamov@gmail.com>
2013-08-21 14:08:15 +0600

    added ApplicationListItemMapper, PathConstants

---
+++ Alicargo/Helpers/PathConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Alicargo.Helpers
+{
+    public static class PathConstants
+    {
+        public const string ScriptsPath = "~/js";
+        public const string ScriptsPathRu = "~/js/ru";
+        public const string ScriptsPathIt = "~/js/it";
+        public const string StylesPath = "~/css";
+    }
+}
