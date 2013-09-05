a039ef5bfb07e7b03d75a60f3559bb1cc8201557
S <6alyamov@gmail.com>
2013-09-05 22:58:26 +0400

    clients grid refactored dynamic localization script refactored

---
+++ Alicargo/Scripts/app/Client/Grid.js
@@ -0,0 +1,50 @@
+﻿$(function () {
+	var schema = {
+		model: {
+			id: "Id"
+		},
+		data: "Data",
+		total: "Total"
+	};
+
+	var error = function () { alert(Alicargo.Localization.Pages_AnError); };
+
+	var dataSource = {
+		transport: {
+			read: {
+				dataType: "json",
+				url: Alicargo.Urls.Client_List,
+				type: "POST"
+			}
+		},
+		schema: schema,
+		pageSize: 20,
+		serverPaging: true,
+		editable: true,
+		error: error
+	};
+
+	$("#client-grid").kendoGrid({
+		dataSource: dataSource,
+		filterable: false,
+		sortable: false,
+		pageable: true,
+		editable: false,
+		columns: [
+			{ field: "LegalEntity", title: Alicargo.Localization.Entities_LegalEntity },
+			{ field: "Nic", title: Alicargo.Localization.Entities_Nic },
+		{ field: "Email", title: Alicargo.Localization.Entities_Email },
+			{
+				command: [{
+					name: "custom-edit",
+					text: "",
+					click: function (e) {
+						var tr = $(e.target).closest("tr");
+						var data = this.dataItem(tr);
+						var url = Alicargo.Urls.Client_Edit + "/" + data.Id;
+						window.location = url;
+					}
+				}], title: "&nbsp;", width: "53px"
+			}]
+	});
+});
