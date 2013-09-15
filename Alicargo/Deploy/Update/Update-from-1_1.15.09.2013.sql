1e943c4f26ce520ec4f3522b8b6b9557103367a2
S <6alyamov@gmail.com>
2013-09-15 22:24:05 +0400

    added update script

---
+++ Alicargo/Deploy/Update/Update-from-1_1.15.09.2013.sql
@@ -0,0 +1,39 @@
+﻿GO
+ALTER TABLE [dbo].[AirWaybill] DROP COLUMN [ForwarderCost];
+
+
+GO
+ALTER TABLE [dbo].[AirWaybill]
+    ADD [TotalCostOfSenderForWeight] MONEY NULL;
+
+
+GO
+PRINT N'Altering [dbo].[Application]...';
+
+
+GO
+ALTER TABLE [dbo].[Application]
+    ADD [FactureCost]        MONEY NULL,
+        [ScotchCost]         MONEY NULL,
+        [WithdrawCost]       MONEY NULL,
+        [FactureCostEdited]  MONEY NULL,
+        [ScotchCostEdited]   MONEY NULL,
+        [WithdrawCostEdited] MONEY NULL,
+        [ForwarderCost]      MONEY NULL,
+        [TariffPerKg]        MONEY NULL,
+        [TransitCost]        MONEY NULL;
+
+
+GO
+PRINT N'Altering [dbo].[Client]...';
+
+
+GO
+ALTER TABLE [dbo].[Client]
+    ADD [ContractFileData] VARBINARY (MAX) NULL,
+        [ContractFileName] NVARCHAR (MAX)  NULL;
+
+
+GO
+PRINT N'Update complete.'
+GO
