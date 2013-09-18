79358ed236145325c44080daa7d87bb52453eda8
S <6alyamov@gmail.com>
2013-09-18 22:59:29 +0400

    added PROCEDURE [dbo].[Client_DeleteForce]

---
+++ Alicargo.Database/Stored procedures/Client_DeleteForce.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[Client_DeleteForce]
+	@ClientId BIGINT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE	@UserId BIGINT, @TransitId BIGINT;
+	SELECT TOP(1) @UserId = c.[UserId], @TransitId = c.[TransitId]
+	FROM [dbo].[Client] c
+	WHERE c.Id = @ClientId
+
+	BEGIN TRAN
+		
+		DELETE
+		FROM	[dbo].[Application]
+		WHERE	[ClientId] = @ClientId
+
+		DELETE	TOP(1)
+		FROM	[dbo].[Client]
+		WHERE	[Id] = @ClientId
+
+		DELETE	TOP(1)
+		FROM	[dbo].[Transit]
+		WHERE	[Id] = @TransitId
+
+		DELETE	TOP(1)
+		FROM	[dbo].[User]
+		WHERE	[Id] = @UserId
+
+	COMMIT
+END
