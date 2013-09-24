033718f3b2b4d53b238d063f6455f729a0db5bc1
S <6alyamov@gmail.com>
2013-09-24 09:29:28 +0400

    dev

--- Alicargo/Deploy/Update/Update-from-1_1.15.09.2013.sql
+++ Alicargo/Deploy/Update/Update-from-1_1.15.09.2013.sql
@@ -21,7 +21,8 @@ ALTER TABLE [dbo].[Application]
         [WithdrawCostEdited] MONEY NULL,
         [ForwarderCost]      MONEY NULL,
         [TariffPerKg]        MONEY NULL,
-        [TransitCost]        MONEY NULL;
+        [TransitCost]        MONEY NULL,
+		[SenderId]           BIGINT NULL;
 
 
 GO
@@ -35,5 +36,39 @@ ALTER TABLE [dbo].[Client]
 
 
 GO
+CREATE PROCEDURE [dbo].[Client_DeleteForce]
+	@ClientId BIGINT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE	@UserId BIGINT, @TransitId BIGINT;
+	SELECT TOP(1) @UserId = c.[UserId], @TransitId = c.[TransitId]
+	FROM [dbo].[Client] c
+	WHERE c.Id = @ClientId
+
+	BEGIN TRAN
+		
+		DELETE
+		FROM	[dbo].[Application]
+		WHERE	[ClientId] = @ClientId
+
+		DELETE	TOP(1)
+		FROM	[dbo].[Client]
+		WHERE	[Id] = @ClientId
+
+		DELETE	TOP(1)
+		FROM	[dbo].[Transit]
+		WHERE	[Id] = @TransitId
+
+		DELETE	TOP(1)
+		FROM	[dbo].[User]
+		WHERE	[Id] = @UserId
+
+	COMMIT
+END
+GO
+
+
 PRINT N'Update complete.'
 GO
