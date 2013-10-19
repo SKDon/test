cda318bd0686f1fa4267397026934f3ba67b1113
S <6alyamov@gmail.com>
2013-10-19 19:09:56 +0400

    added SqlProcedureExecutor

---
+++ Alicargo.Contracts/Repositories/ISqlProcedureExecutor.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Alicargo.Contracts.Repositories
+{
+	public interface ISqlProcedureExecutor
+	{
+		T Get<T>(string sql, object param = null, IDbTransaction transaction = null);
+	}
+}
---
+++ Alicargo.DataAccess/DbContext/SqlProcedureExecutor.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Alicargo.Contracts.Repositories;
+using Dapper;
+
+namespace Alicargo.DataAccess.DbContext
+{
+	public sealed class SqlProcedureExecutor : ISqlProcedureExecutor
+	{
+		private readonly string _connectionString;
+
+		public SqlProcedureExecutor(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		public T Get<T>(string sql, object param = null, IDbTransaction transaction = null)
+		{
+			using (var connection = new SqlConnection(_connectionString))
+			{
+				return connection.Query<T>(sql, param, transaction, commandType: CommandType.StoredProcedure).FirstOrDefault();
+			}
+		}
+	}
+}
