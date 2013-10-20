290c01c9a50ac0642a6d7dd7d1477898852b8f2d
S <6alyamov@gmail.com>
2013-10-20 16:32:05 +0400

    minor

--- Alicargo/Scripts/app/Application/Grid.js
+++ Alicargo/Scripts/app/Application/Grid.js
@@ -1,4 +1,4 @@
-﻿$(function () {
+﻿$(function() {
 
 	var $a = Alicargo;
 	var $u = $a.Urls;
@@ -6,141 +6,155 @@
 	var $r = $a.Roles;
 	var $s = $a.States;
 
-	$a.Application = (function ($apl) {
-		$apl.GetGrid = function () {
+	$a.Application = (function($apl) {
+		$apl.GetGrid = function() {
 			var grid = $("#application-grid").data("kendoGrid");
-			$apl.GetGrid = function () { return grid; };
+			$apl.GetGrid = function() { return grid; };
 			return $apl.GetGrid();
 		};
 
-		$apl.UpdateGrid = function () {
+		$apl.UpdateGrid = function() {
 			var grid = $apl.GetGrid();
 			grid.dataSource.read();
 			grid.refresh();
 		};
 
+		function getDataSource() {
+			var schema = {
+				model: { id: "Id" },
+				data: "Data",
+				total: "Total",
+				groups: "Groups"
+			};
+
+			var dataSource = {
+				transport: {
+					read: {
+						dataType: "json",
+						url: $u.ApplicationList_List,
+						type: "POST",
+						cache: false
+					}
+				},
+				schema: schema,
+				pageSize: 20,
+				serverPaging: true,
+				serverGrouping: true,
+				error: $a.ShowError,
+				group: { field: "AirWaybill", dir: "asc" }
+			};
+
+			if (!$r.IsClient) {
+				$.extend(dataSource, { groupable: true });
+			}
+
+			return dataSource;
+		}
+
+		function getSettings() {
+			var settings = {
+				dataSource: getDataSource(),
+				filterable: false,
+				sortable: false,
+				groupable: false,
+				pageable: { refresh: true, pageSizes: [10, 20, 50, 100] },
+				resizable: true,
+				columns: $a.Application.GetColumns()
+			};
+
+			var detailExpand = function(e) {
+				var data = $a.Application.GetGrid().dataItem(e.masterRow);
+
+				$(".close-application", e.detailRow).click(function() {
+					if (window.confirm($l.Pages_StateSetConfirm)) {
+						var url = $u.ApplicationUpdate_Close;
+						$.post(url, { id: data.Id }).done($a.Application.UpdateGrid).fail($a.ShowError);
+					}
+				});
+
+				$(".delete-application", e.detailRow).click(function() {
+					if (window.confirm($l.Pages_DeleteConfirm)) {
+						var url = $u.Application_Delete;
+						$.post(url, { id: data.Id }).done($a.Application.UpdateGrid).fail($a.ShowError);
+					}
+				});
+
+				$(".AirWaybill-select", e.detailRow)
+					.val(data.AirWaybillId)
+					.change(function() {
+						if (window.confirm($l.Pages_AirWaybillSetConfirm)) {
+							var url = $u.AirWaybill_SetAirWaybill;
+							$.post(url, { AirWaybillId: $(this).val(), applicationId: data.Id }).done($a.Application.UpdateGrid).fail($a.ShowError);
+						}
+					});
+
+				if (data.AirWaybillId && data.StateId != $s.CargoIsFlewStateId) {
+					$(".AirWaybill-holder", e.detailRow).hide();
+				} else {
+					$(".AirWaybill-holder", e.detailRow).show();
+				}
+			};
+
+			var detailCollapse = function(e) {
+				$(".close-application", e.detailRow).unbind("click");
+				$(".delete-application", e.detailRow).unbind("click");
+				$(".AirWaybill-select", e.detailRow).unbind("change");
+			};
+
+			if (!$r.IsForwarder) {
+				$.extend(settings, {
+					detailTemplate: kendo.template($("#application-grid-details").html()),
+					detailExpand: detailExpand,
+					detailCollapse: detailCollapse
+				});
+			}
+
+			if ($r.IsClient) {
+				$.extend(settings, {
+					dataBound: function() {
+						this.expandRow(this.tbody.find("tr.k-master-row"));
+					}
+				});
+			}
+
+			if (!$r.IsClient && !$r.IsBroker)
+				$.extend(settings, {
+					editable: true,
+					edit: function(e) {
+						if (e.container.find("input[name='TransitReference']").length) return;
+						if (e.container.find("input[name='DateOfCargoReceiptLocalString']").length) return;
+						if (($r.IsForwarder || $r.IsAdmin) && e.container.find("input[name='TransitCost']").length) return;
+
+						var stateName = e.container.find("input[data-text-field='StateName']");
+						if (!stateName.length || !e.model.CanSetState) $a.Application.GetGrid().closeCell();
+					},
+					save: function(e) {
+						if (e.values.TransitCost !== undefined) {
+							$.post($u.ApplicationUpdate_SetTransitCost, {
+								id: e.model.Id,
+								transitCost: e.values.TransitCost
+							}).fail($a.ShowError);
+						}
+						if (e.values.TransitReference !== undefined) {
+							$.post($u.ApplicationUpdate_SetTransitReference, {
+								id: e.model.Id,
+								TransitReference: e.values.TransitReference
+							}).fail($a.ShowError);
+						}
+						if (e.values.DateOfCargoReceiptLocalString !== undefined) {
+							$.post($u.ApplicationUpdate_SetDateOfCargoReceipt, {
+								id: e.model.Id,
+								dateOfCargoReceipt: kendo.toString(e.values.DateOfCargoReceiptLocalString, "d")
+							}).fail($a.ShowError);
+						}
+					}
+				});
+
+			return settings;
+		}
+
+		$("#application-grid").kendoGrid(getSettings());
+
 		return $apl;
 	})($a.Application || {});
-
-	var schema = {
-		model: { id: "Id" },
-		data: "Data",
-		total: "Total",
-		groups: "Groups"
-	};
-
-	var dataSource = {
-		transport: {
-			read: {
-				dataType: "json",
-				url: $u.ApplicationList_List,
-				type: "POST",
-				cache: false
-			}
-		},
-		schema: schema,
-		pageSize: 20,
-		serverPaging: true,
-		serverGrouping: true,
-		error: $a.ShowError
-	};
-
-
-	var detailExpand = function (e) {
-		var data = $a.Application.GetGrid().dataItem(e.masterRow);
-
-		$(".close-application", e.detailRow).click(function () {
-			if (window.confirm($l.Pages_StateSetConfirm)) {
-				var url = $u.ApplicationUpdate_Close;
-				$.post(url, { id: data.Id }).done($a.Application.UpdateGrid).fail($a.ShowError);
-			}
-		});
-
-		$(".delete-application", e.detailRow).click(function () {
-			if (window.confirm($l.Pages_DeleteConfirm)) {
-				var url = $u.Application_Delete;
-				$.post(url, { id: data.Id }).done($a.Application.UpdateGrid).fail($a.ShowError);
-			}
-		});
-
-		$(".AirWaybill-select", e.detailRow)
-			.val(data.AirWaybillId)
-			.change(function () {
-				if (window.confirm($l.Pages_AirWaybillSetConfirm)) {
-					var url = $u.AirWaybill_SetAirWaybill;
-					$.post(url, { AirWaybillId: $(this).val(), applicationId: data.Id }).done($a.Application.UpdateGrid).fail($a.ShowError);
-				}
-			});
-
-		if (data.AirWaybillId && data.StateId != $s.CargoIsFlewStateId) {
-			$(".AirWaybill-holder", e.detailRow).hide();
-		} else {
-			$(".AirWaybill-holder", e.detailRow).show();
-		}
-	};
-	var detailCollapse = function (e) {
-		$(".close-application", e.detailRow).unbind("click");
-		$(".delete-application", e.detailRow).unbind("click");
-		$(".AirWaybill-select", e.detailRow).unbind("change");
-	};
-
-	var settings = {
-		dataSource: dataSource,
-		filterable: false,
-		sortable: false,
-		groupable: false,
-		pageable: { refresh: true, pageSizes: [10, 20, 50, 100] },
-		resizable: true,
-		columns: $a.Application.GetColumns()
-	};
-
-	if (!$r.IsForwarder) {
-		$.extend(settings, {
-			detailTemplate: kendo.template($("#application-grid-details").html()),
-			detailExpand: detailExpand,
-			detailCollapse: detailCollapse
-		});
-	}
-
-	if (!$r.IsClient) {
-		$.extend(dataSource, {
-			group: { field: "AirWaybill", dir: "asc" },
-			groupable: true
-		});
-	}
-
-	if (!$r.IsClient && !$r.IsBroker)
-		$.extend(settings, {
-			editable: true,
-			edit: function (e) {
-				if (e.container.find("input[name='TransitReference']").length) return;
-				if (e.container.find("input[name='DateOfCargoReceiptLocalString']").length) return;
-				if (($r.IsForwarder || $r.IsAdmin) && e.container.find("input[name='TransitCost']").length) return;
-
-				var stateName = e.container.find("input[data-text-field='StateName']");
-				if (!stateName.length || !e.model.CanSetState) $a.Application.GetGrid().closeCell();
-			},
-			save: function (e) {
-				if (e.values.TransitCost !== undefined) {
-					$.post($u.ApplicationUpdate_SetTransitCost, {
-						id: e.model.Id,
-						transitCost: e.values.TransitCost
-					}).fail($a.ShowError);
-				}
-				if (e.values.TransitReference !== undefined) {
-					$.post($u.ApplicationUpdate_SetTransitReference, {
-						id: e.model.Id,
-						TransitReference: e.values.TransitReference
-					}).fail($a.ShowError);
-				}
-				if (e.values.DateOfCargoReceiptLocalString !== undefined) {
-					$.post($u.ApplicationUpdate_SetDateOfCargoReceipt, {
-						id: e.model.Id,
-						dateOfCargoReceipt: kendo.toString(e.values.DateOfCargoReceiptLocalString, "d")
-					}).fail($a.ShowError);
-				}
-			}
-		});
-
-	$("#application-grid").kendoGrid(settings);
 });
