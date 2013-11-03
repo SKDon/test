865756d812ae7c2ae8d992c0dc60f379ba8d5a9f
6alyamov <6alyamov@gmail.com>
2013-11-03 13:05:39 +0400

    fixed tests

--- Alicargo.Tests/Services/Application/ApplicationListPresenterTests.cs
+++ Alicargo.Tests/Services/Application/ApplicationListPresenterTests.cs
@@ -12,25 +12,25 @@ using Moq;
 
 namespace Alicargo.Tests.Services.Application
 {
-	[TestClass]
-	public class ApplicationListPresenterTests
-	{
-		private MockContainer _context;
-		private IApplicationListPresenter _service;
+    [TestClass]
+    public class ApplicationListPresenterTests
+    {
+        private MockContainer _context;
+        private IApplicationListPresenter _service;
 
-		[TestInitialize]
-		public void TestInitialize()
-		{
-			_context = new MockContainer();
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _context = new MockContainer();
 
-			_service = _context.Create<ApplicationListPresenter>();
-		}
+            _service = _context.Create<ApplicationListPresenter>();
+        }
 
-		[TestMethod]
-		public void Test_List()
-		{
-			var states = new[] { 0L };
-			var orders = new[]
+        [TestMethod]
+        public void Test_List()
+        {
+            var states = new[] { 0L };
+            var orders = new[]
                 {
                     new Order
                         {
@@ -38,33 +38,27 @@ namespace Alicargo.Tests.Services.Application
                             Desc = false
                         }
                 };
-			const long clientId = 1;
-			var data = _context.CreateMany<ApplicationListItemData>().ToArray();
-			var map = _context.CreateMany<ApplicationListItem>().ToArray();
-			_context.StateService.Setup(x => x.GetVisibleStates()).Returns(states);
-			_context.ApplicationListItemMapper.Setup(x => x.Map(data)).Returns(map);
-			_context.ApplicationGrouper.Setup(x => x.Group(map, new[] { OrderType.LegalEntity }))
-					.Returns(new ApplicationGroup[0]);
-			_context.ApplicationRepository.Setup(x => x.Count(states, clientId, null)).Returns(It.IsAny<long>());
-			_context.ApplicationRepository.Setup(
-				x =>
-				x.List(0, 0, states,
-					   It.Is<Order[]>(y => y.Last().OrderType == OrderType.Id && y.Last().Desc),
-					   clientId, null)).Returns(data);
+            const long clientId = 1;
+            var data = _context.CreateMany<ApplicationListItemData>().ToArray();
+            var map = _context.CreateMany<ApplicationListItem>().ToArray();
+            _context.StateService.Setup(x => x.GetVisibleStates()).Returns(states);
+            _context.ApplicationListItemMapper.Setup(x => x.Map(data)).Returns(map);
+            _context.ApplicationGrouper.Setup(x => x.Group(map, new[] { OrderType.LegalEntity }))
+                    .Returns(new ApplicationGroup[0]);
+            _context.ApplicationRepository.Setup(x => x.Count(states, clientId, null)).Returns(It.IsAny<long>());
+            _context.ApplicationRepository.Setup(x => x.List(0, 0, states, orders, clientId, null)).Returns(data);
 
-			var collection = _service.List(0, 0, orders, clientId);
+            var collection = _service.List(0, 0, orders, clientId);
 
-			collection.Data.Should().BeNull();
-			collection.Groups.Should().NotBeNull();
+            collection.Data.Should().BeNull();
+            collection.Groups.Should().NotBeNull();
 
-			_context.StateService.Verify(x => x.GetVisibleStates(), Times.Once());
-			_context.ApplicationListItemMapper.Verify(x => x.Map(data));
-			_context.ApplicationGrouper.Verify(x => x.Group(map, new[] { OrderType.LegalEntity }), Times.Once());
-			_context.ApplicationRepository.Verify(x => x.Count(states, clientId, null), Times.Once());
-			_context.ApplicationRepository.Verify(x =>
-				x.List(0, 0, states,
-					   It.Is<Order[]>(y => y.Last().OrderType == OrderType.Id && y.Last().Desc),
-					   clientId, null), Times.Once());
-		}
-	}
+            _context.StateService.Verify(x => x.GetVisibleStates(), Times.Once());
+            _context.ApplicationListItemMapper.Verify(x => x.Map(data));
+            _context.ApplicationGrouper.Verify(x => x.Group(map, new[] { OrderType.LegalEntity }), Times.Once());
+            _context.ApplicationRepository.Verify(x => x.Count(states, clientId, null), Times.Once());
+            _context.ApplicationRepository.Verify(x =>
+                x.List(0, 0, states, orders, clientId, null), Times.Once());
+        }
+    }
 }
