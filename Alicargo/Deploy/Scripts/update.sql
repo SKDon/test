80a791223df79f06d80351e03a7d4bfdfea3facb
6alyamov <6alyamov@gmail.com>
2013-11-12 23:38:49 +0400

    Dev build configuration

--- Alicargo/Deploy/Scripts/update.sql
+++ Alicargo/Deploy/Scripts/update.sql
@@ -1,1 +1,38 @@
-﻿
+﻿USE [master] 
+GO
+
+SET ANSI_NULLS ON 
+GO
+
+SET QUOTED_IDENTIFIER ON 
+GO
+ 
+ALTER PROCEDURE [dbo].[sp_RestoreDatabase]
+	@newDb NVARCHAR(50),
+	@oldDb NVARCHAR(50),
+	@fromFile NVARCHAR(200),
+	@dataFolder NVARCHAR(200)
+AS
+
+BEGIN
+ 
+	SET NOCOUNT ON;
+
+	DECLARE @sqlRestoreDb NVARCHAR(1000) 
+		= N'RESTORE DATABASE ' + @newDb + N' FROM '
+		+ N'DISK = N''' + @fromFile + N''' WITH FILE = 1, '
+		+ N'MOVE N''' + @oldDb + N''' TO N''' + @dataFolder +  @newDb + N'.mdf'', '
+		+ N'MOVE N''' + @oldDb + N'_log'' TO N''' + @dataFolder + @newDb + N'_log.ldf'', '
+		+ N'NOUNLOAD, REPLACE, STATS = 5;'
+		+ N'ALTER DATABASE ' + @newDb + N' MODIFY FILE (NAME=N'''+ @oldDb + N''', NEWNAME=N''' + @newDb + N''');'
+		+ N'ALTER DATABASE ' + @newDb + N' MODIFY FILE (NAME=N'''+ @oldDb + N'_log'', NEWNAME=N''' + @newDb + N'_log'');'
+
+	EXEC(@sqlRestoreDb)
+
+	PRINT @newDb + N' have been restored.'
+
+END
+GO
+
+PRINT N'[sp_RestoreDatabase] updated...'
+GO
---
+++ Alicargo/Deploy/restore-previous-release.sql
@@ -0,0 +1,8 @@
+﻿USE [master]
+GO
+
+exec [dbo].[sp_RestoreDatabase] 'Alicargo_2_1', 'Alicargo', 'A:\Projects\Alicargo_2_1_11122013_182701.bak', 'A:\Program Files\Microsoft SQL Server\MSSQL10_50.SQLEXPRESS\MSSQL\DATA\'
+GO
+
+exec [dbo].[sp_RestoreDatabase] 'Alicargo_Files_2_1', 'Alicargo_Files_2_0', 'A:\Projects\Alicargo_Files_2_1_11122013_182706.bak', 'A:\Program Files\Microsoft SQL Server\MSSQL10_50.SQLEXPRESS\MSSQL\DATA\'
+GO
