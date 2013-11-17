fed02b787be4882f2f1eab2b8dfc9223b46f09b5
6alyamov <6alyamov@gmail.com>
2013-11-17 14:00:51 +0400

    added [StateEmailRecipient] table

---
+++ Alicargo.Database/Tables/StateEmailRecipient.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[StateEmailRecipient]
+(
+	[RoleId] INT NOT NULL,
+	[StateId] BIGINT NOT NULL,
+
+	CONSTRAINT [PK_dbo.StateEmailRecipient] PRIMARY KEY CLUSTERED ([RoleId] ASC, [StateId] ASC),
+	CONSTRAINT [FK_dbo.StateEmailRecipient_dbo.State_StateId] FOREIGN KEY ([StateId]) REFERENCES [dbo].[State] ([Id]) ON DELETE CASCADE
+);
+GO
