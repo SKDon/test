d9620160b47dd14a022b2e33d23da6cd3d23a897
6alyamov <6alyamov@gmail.com>
2013-11-24 13:12:19 +0400

    added EmailTemplateController

---
+++ Alicargo/Controllers/EmailTemplateController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Alicargo.Contracts.Enums;
+using Alicargo.Core.Localization;
+using Alicargo.MvcHelpers.Filters;
+
+namespace Alicargo.Controllers
+{
+	public class EmailTemplateController : Controller
+	{
+		[HttpGet]
+		[Access(RoleType.Admin)]
+		public ViewResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost, Access(RoleType.Admin),
+		 OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
+		public JsonResult List()
+		{
+			var types = Enum.GetValues(typeof(ApplicationEventType))
+				.Cast<ApplicationEventType>()
+				.Select(x => new
+				{
+					Id = (int)x,
+					Name = x.ToLocalString()
+				})
+				.ToArray();
+
+			return Json(types);
+		}
+	}
+}
