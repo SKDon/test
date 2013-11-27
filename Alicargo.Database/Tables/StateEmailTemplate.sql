bf57a07d83c3c771a94e68b867a1e791947e8b9b
6alyamov <6alyamov@gmail.com>
2013-11-28 00:54:48 +0400

    updated templates SP

--- Alicargo.Database/Tables/StateEmailTemplate.sql
+++ Alicargo.Database/Tables/StateEmailTemplate.sql
@@ -1,18 +1,14 @@
 ﻿CREATE TABLE [dbo].[StateEmailTemplate]
 (
-	[Id] BIGINT IDENTITY(1, 1) NOT NULL CONSTRAINT [PK_dbo.StateEmailTemplate] PRIMARY KEY CLUSTERED ([Id] ASC),
-
 	[StateId] BIGINT NOT NULL,
 	[EmailTemplateId] BIGINT NOT NULL,
 
 	[EnableEmailSend] BIT NOT NULL CONSTRAINT [DF_StateEmailTemplate_EnableEmailSend] DEFAULT 1,
 	[UseApplicationEventTemplate] BIT NOT NULL CONSTRAINT [DF_StateEmailTemplate_UseApplicationEventTemplate] DEFAULT 0,
 	
+	CONSTRAINT [PK_dbo.StateEmailTemplate] PRIMARY KEY CLUSTERED ([EmailTemplateId] ASC, [StateId] ASC),
 	CONSTRAINT [FK_dbo.StateEmailTemplate_dbo.State_StateId] FOREIGN KEY ([StateId]) REFERENCES [dbo].[State] ([Id]) ON DELETE CASCADE,
 	CONSTRAINT [FK_dbo.StateEmailTemplate_dbo.EmailTemplate_EmailTemplateId] FOREIGN KEY
 		([EmailTemplateId]) REFERENCES [dbo].[EmailTemplate] ([Id]) ON DELETE CASCADE
 );
-GO
-
-CREATE UNIQUE INDEX [IX_StateEmailTemplate_StateId_EmailTemplateId] ON [dbo].[StateEmailTemplate] ([StateId], [EmailTemplateId])
 GO
