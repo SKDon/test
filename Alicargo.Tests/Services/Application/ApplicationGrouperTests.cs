fc2daac458fb1222c89285549443f1b1986cdf92
6alyamov <6alyamov@gmail.com>
2013-12-15 20:26:31 +0400

    fix a test

--- Alicargo.Tests/Services/Application/ApplicationGrouperTests.cs
+++ Alicargo.Tests/Services/Application/ApplicationGrouperTests.cs
@@ -14,72 +14,124 @@ namespace Alicargo.Tests.Services.Application
 	[TestClass]
 	public class ApplicationGrouperTests
 	{
-		[TestMethod]
-		public void Test_ApplicationGrouper_Group()
+		private Fixture _fixture;
+		private Mock<IAwbRepository> _awbRepository;
+		private long[] _awbIds;
+		private AirWaybillData[] _awbsData;
+		private ApplicationListItem[] _applications;
+		private ApplicationGrouper _grouper;
+		const int AppCount = 99;
+		const int ClientCount = 6;
+		const int AWBCount = 9;
+
+		[TestInitialize]
+		public void TestInitialize()
 		{
-			var fixture = new Fixture();
-			var awbRepository = new Mock<IAwbRepository>(MockBehavior.Strict);
-
-			const int appCount = 99;
-			const int clientCount = 6;
-			const int awbCount = 9;
-
-			var awbIds = Enumerable.Range(0, awbCount).Select(x => (long) x).ToArray();
-			var awbsData = awbIds.Select(i => fixture.Build<AirWaybillData>()
+			_fixture = new Fixture();
+			_awbRepository = new Mock<IAwbRepository>(MockBehavior.Strict);
+			_awbIds = Enumerable.Range(0, AWBCount).Select(x => (long)x).ToArray();
+			_awbsData = _awbIds.Select(i => _fixture.Build<AirWaybillData>()
 				.With(d => d.Id, i)
 				.Create()).ToArray();
-			awbRepository.Setup(x => x.Get(awbIds)).Returns(awbsData);
-			awbRepository.Setup(x => x.GetAggregate(awbIds))
-				.Returns(awbIds.Select(x => new AirWaybillAggregate
-				{
-					AirWaybillId = x,
-					StateId = 0,
-					TotalCount = (int) x,
-					TotalWeight = x
-				}).ToArray());
-			awbRepository.Setup(x => x.GetTotalCountWithouAwb()).Returns((int?) null);
-			awbRepository.Setup(x => x.GetTotalWeightWithouAwb()).Returns((float?) null);
+			_awbRepository.Setup(x => x.Get(_awbIds)).Returns(_awbsData);
 
-			var grouper = new ApplicationGrouper(awbRepository.Object);
-
-			var applications = fixture.CreateMany<ApplicationListItem>(appCount).ToArray();
-			for (var i = 0; i < appCount; i++)
+			_applications = _fixture.CreateMany<ApplicationListItem>(AppCount).ToArray();
+			for (var i = 0; i < AppCount; i++)
 			{
-				var item = applications[i];
-				item.AirWaybillId = i%awbCount;
+				var item = _applications[i];
+				item.AirWaybillId = i % AWBCount;
 				item.Id = i;
-				item.ClientLegalEntity = "Client " + i%clientCount;
+				item.ClientLegalEntity = "Client " + i % ClientCount;
 				item.Count = i;
 				item.Weight = i;
 				item.Value = i;
 				item.Volume = i;
 			}
 
-			var groups = grouper.Group(applications, new[]
+			_grouper = new ApplicationGrouper(_awbRepository.Object);
+		}
+
+		[TestMethod]
+		public void Test_ApplicationGrouper_GroupWithAirWaybill()
+		{
+			_awbRepository.Setup(x => x.GetAggregate(_awbIds))
+				.Returns(_awbIds.Select(x => new AirWaybillAggregate
+				{
+					AirWaybillId = x,
+					StateId = 0,
+					TotalCount = (int)x,
+					TotalWeight = x,
+					TotalValue = x,
+					TotalVolume = x
+				}).ToArray());
+			_awbRepository.Setup(x => x.GetTotalCountWithouAwb()).Returns((int?)null);
+			_awbRepository.Setup(x => x.GetTotalWeightWithouAwb()).Returns((float?)null);
+			_awbRepository.Setup(x => x.GetTotalValueWithouAwb()).Returns(0);
+			_awbRepository.Setup(x => x.GetTotalVolumeWithouAwb()).Returns(0);
+
+			var groups = _grouper.Group(_applications, new[]
 			{
 				OrderType.AirWaybill,
 				OrderType.LegalEntity
 			});
 
-			groups.Count().ShouldBeEquivalentTo(awbCount);
+			groups.Count().ShouldBeEquivalentTo(AWBCount);
 
 			for (var index = 0; index < groups.Length; index++)
 			{
 				var @group = groups[index];
 				@group.aggregates.Count.sum.ShouldBeEquivalentTo(index);
 				@group.aggregates.Weight.sum.ShouldBeEquivalentTo(index);
+				@group.aggregates.Value.sum.ShouldBeEquivalentTo(index);
+				@group.aggregates.Volume.sum.ShouldBeEquivalentTo(index);
 				@group.hasSubgroups.ShouldBeEquivalentTo(true);
 				foreach (ApplicationGroup item in @group.items)
 				{
 					item.hasSubgroups.ShouldBeEquivalentTo(false);
 					item.items.Count()
-						.Should().BeLessOrEqualTo(clientCount)
-						.And.BeGreaterOrEqualTo(clientCount - 1);
+						.Should().BeLessOrEqualTo(ClientCount)
+						.And.BeGreaterOrEqualTo(ClientCount - 1);
 				}
 			}
+		}
 
-			awbRepository.Verify(x => x.GetTotalCountWithouAwb(), Times.Once);
-			awbRepository.Verify(x => x.GetTotalWeightWithouAwb(), Times.Once);
+		[TestMethod]
+		public void Test_ApplicationGrouper_GroupWithoutAirWaybill()
+		{
+			var count = _fixture.Create<int>();
+			var weidht = _fixture.Create<float>();
+			var value = _fixture.Create<decimal>();
+			var volume = _fixture.Create<float>();
+			_awbRepository.Setup(x => x.GetAggregate(_awbIds)).Returns(new AirWaybillAggregate[0]);
+			_awbRepository.Setup(x => x.GetTotalCountWithouAwb()).Returns(count);
+			_awbRepository.Setup(x => x.GetTotalWeightWithouAwb()).Returns(weidht);
+			_awbRepository.Setup(x => x.GetTotalValueWithouAwb()).Returns(value);
+			_awbRepository.Setup(x => x.GetTotalVolumeWithouAwb()).Returns(volume);
+
+			var groups = _grouper.Group(_applications, new[]
+			{
+				OrderType.AirWaybill,
+				OrderType.LegalEntity
+			});
+
+			groups.Count().ShouldBeEquivalentTo(AWBCount);
+
+			for (var index = 0; index < groups.Length; index++)
+			{
+				var @group = groups[index];
+				@group.aggregates.Count.sum.ShouldBeEquivalentTo(count);
+				@group.aggregates.Weight.sum.ShouldBeEquivalentTo(weidht);
+				@group.aggregates.Value.sum.ShouldBeEquivalentTo(value);
+				@group.aggregates.Volume.sum.ShouldBeEquivalentTo(volume);
+				@group.hasSubgroups.ShouldBeEquivalentTo(true);
+				foreach (ApplicationGroup item in @group.items)
+				{
+					item.hasSubgroups.ShouldBeEquivalentTo(false);
+					item.items.Count()
+						.Should().BeLessOrEqualTo(ClientCount)
+						.And.BeGreaterOrEqualTo(ClientCount - 1);
+				}
+			}
 		}
 	}
 }
