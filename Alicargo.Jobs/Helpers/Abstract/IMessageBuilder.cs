2d791637c639833c3714100cf3b8e2366e322339
6alyamov <6alyamov@gmail.com>
2014-01-03 18:02:06 +0400

    minor

---
+++ Alicargo.Jobs/Helpers/Abstract/IMessageBuilder.cs
@@ -0,0 +1,10 @@
+﻿using Alicargo.Contracts.Contracts;
+using Alicargo.Contracts.Enums;
+
+namespace Alicargo.Jobs.Helpers.Abstract
+{
+	public interface IMessageBuilder
+	{
+		EmailMessage[] Get(EventType type, EventData eventData);
+	}
+}
