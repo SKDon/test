3cba95bf8948e3bfa2cbf18a8bac9b0a8c490b3b
6alyamov <6alyamov@gmail.com>
2014-01-06 15:36:43 +0400

    added Alicargo.Jobs.BlackBox.Tests

---
+++ Alicargo.Jobs.BlackBox.Tests/ApplicationEvents/Helpers/MessageBuilderTests.cs
@@ -0,0 +1,34 @@
+﻿using Alicargo.App_Start.Jobs;
+using Alicargo.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+
+namespace Alicargo.Jobs.BlackBox.Tests.ApplicationEvents.Helpers
+{
+	[TestClass]
+	public class MessageBuilderTests
+	{
+		private DbTestContext _context;
+		private Fixture _fixture;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_context = new DbTestContext(Settings.Default.MainConnectionString);
+			_fixture = new Fixture();
+		}
+
+		[TestCleanup]
+		public void TestCleanup()
+		{
+			_context.Cleanup();
+		}
+
+		[TestMethod]
+		[TestCategory("black-box")]
+		public void Test_Get()
+		{
+			//CompositionJobsHelper.GetMessageFactory()
+		}
+	}
+}
