0c2a8b1be1c6c1957d7d1c7f762293f8820f8b95
6alyamov <6alyamov@gmail.com>
2014-01-08 12:56:23 +0400

    ClientExcelHelperTests

---
+++ Alicargo.Jobs.Tests/Helpers/ClientExcelHelperTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Alicargo.Contracts.Contracts.User;
+using Alicargo.Contracts.Enums;
+using Alicargo.Core.Helpers;
+using Alicargo.Jobs.Helpers;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ploeh.AutoFixture;
+
+namespace Alicargo.Jobs.Tests.Helpers
+{
+	[TestClass]
+	public class ClientExcelHelperTests
+	{
+		private Fixture _fixture;
+		private Mock<DateTimeProvider.IDateTimeProvider> _dateTime;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_fixture = new Fixture();
+			_dateTime = new Mock<DateTimeProvider.IDateTimeProvider>(MockBehavior.Strict);
+			_dateTime.SetupGet(x => x.Now).Returns(new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero));
+
+			DateTimeProvider.SetProvider(_dateTime.Object);
+		}
+
+		[TestMethod]
+		public void Test_GetWithInvalidFileNameChars()
+		{
+			var invalidName = Path.GetInvalidFileNameChars().Aggregate("", (s, c) => s + c);
+
+			var clientData = _fixture.Create<ClientData>();
+			clientData.Nic = invalidName;
+			clientData.Language = TwoLetterISOLanguageName.English;
+
+			var actual = ClientExcelHelper.GetName(clientData);
+
+			actual.ShouldBeEquivalentTo("calculation_1_1_2000__________________________________________.xlsx");
+		}
+	}
+}
