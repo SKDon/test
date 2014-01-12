9bc2cfdf9e5a92f5d825711fea4b9fd3c66f7992
6alyamov <6alyamov@gmail.com>
2014-01-12 15:11:19 +0400

    minor

--- Alicargo.Utilities/Properties/AssemblyInfo.cs
+++ Alicargo.Utilities/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4d9cc37d-14aa-4c93-95f0-d4cca43cbc02")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
