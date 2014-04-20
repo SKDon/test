84d130a0dfd402d052b00459f99c3460a59acbef
6alyamov <6alyamov@gmail.com>
2014-04-20 14:14:27 +0400

    added UpdateConflictException

---
+++ Alicargo.DataAccess.Contracts/Exceptions/UpdateConflictException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Alicargo.DataAccess.Contracts.Exceptions
+{
+	public sealed class UpdateConflictException : Exception
+	{
+		public UpdateConflictException(string message) : base(message) { }
+		public UpdateConflictException(string message, Exception innerException) : base(message, innerException) { }
+	}
+}
