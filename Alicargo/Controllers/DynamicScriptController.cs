8a39f2c6cbc16287998e7d69989deab95446b2f3
6alyamov <6alyamov@gmail.com>
2014-04-20 22:59:52 +0400

    added area support to DynamicScriptController

--- Alicargo/Controllers/DynamicScriptController.cs
+++ Alicargo/Controllers/DynamicScriptController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
 using System.Web.UI;
@@ -19,31 +20,13 @@ namespace Alicargo.Controllers
 		}
 
 		[OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
-		public virtual PartialViewResult Roles()
+		public virtual PartialViewResult Localization()
 		{
 			return PartialView();
 		}
 
-		[OutputCache(Duration = int.MaxValue, Location = OutputCacheLocation.ServerAndClient)]
-		public virtual PartialViewResult Urls()
-		{
-			var urs = Assembly.GetExecutingAssembly()
-							  .GetTypes()
-							  .Where(x => x.BaseType == typeof(Controller) && !x.Name.StartsWith("T4MVC_") && x.Name.EndsWith("Controller"))
-							  .Select(x => new ReflectedControllerDescriptor(x))
-							  .SelectMany(x => x.GetCanonicalActions())
-							  .Select(x => new DynamicScriptMethodDescription
-							  {
-								  Action = x.ActionName,
-								  Controller = x.ControllerDescriptor.ControllerName
-							  })
-							  .ToArray();
-
-			return PartialView(urs);
-		}
-
 		[OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
-		public virtual PartialViewResult Localization()
+		public virtual PartialViewResult Roles()
 		{
 			return PartialView();
 		}
@@ -52,11 +35,44 @@ namespace Alicargo.Controllers
 		public virtual PartialViewResult States()
 		{
 			var model = _stateConfig.GetType()
-									.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-									.Where(x => x.PropertyType == typeof(long))
-									.ToDictionary(x => x.Name, x => (long)x.GetValue(_stateConfig));
+				.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+				.Where(x => x.PropertyType == typeof(long))
+				.ToDictionary(x => x.Name, x => (long)x.GetValue(_stateConfig));
 
 			return PartialView(model);
+		}
+
+		[OutputCache(Duration = int.MaxValue, Location = OutputCacheLocation.ServerAndClient)]
+		public virtual PartialViewResult Urls()
+		{
+			var urs = Assembly.GetExecutingAssembly()
+				.GetTypes()
+				.Where(x => x.BaseType == typeof(Controller) && !x.Name.StartsWith("T4MVC_") && x.Name.EndsWith("Controller"))
+				.Select(x => new ReflectedControllerDescriptor(x))
+				.SelectMany(x => x.GetCanonicalActions())
+				.Select(x => new DynamicScriptMethodDescription
+				{
+					Action = x.ActionName,
+					Controller = x.ControllerDescriptor.ControllerName,
+					Area = GetAreaName(x.ControllerDescriptor.ControllerType)
+				})
+				.Distinct()
+				.ToArray();
+
+			return PartialView(urs);
+		}
+
+		private static string GetAreaName(Type controllerType)
+		{
+			var fullName = controllerType.FullName;
+
+			const string areas = "Alicargo.Areas.";
+
+			var controllerName = "." + controllerType.Name + ".Controllers";
+
+			return fullName.StartsWith(areas, StringComparison.InvariantCultureIgnoreCase)
+				? fullName.Remove(fullName.Length - controllerName.Length).Remove(0, areas.Length)
+				: null;
 		}
 	}
 }
