565257d41706cdfbf78960c5e70c54e716adea3e
6alyamov <6alyamov@gmail.com>
2014-04-26 22:17:19 +0400

    added IBillRepository

---
+++ Alicargo.DataAccess.Contracts/Repositories/Application/IBillRepository.cs
@@ -0,0 +1,10 @@
+﻿using Alicargo.DataAccess.Contracts.Contracts.Application;
+
+namespace Alicargo.DataAccess.Contracts.Repositories.Application
+{
+	public interface IBillRepository
+	{
+		void AddOrReplace(BillEditData data);
+		BillEditData Get(long applicationId);
+	}
+}
