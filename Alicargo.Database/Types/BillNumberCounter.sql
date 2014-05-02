a50c952eaa0e3bebd7ae861936c25d54943d15bc
6alyamov <6alyamov@gmail.com>
2014-05-03 01:54:46 +0400

    release

--- Alicargo.Database/Types/BillNumberCounter.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE SEQUENCE [dbo].[BillNumberCounter]
-		AS INT
-		START WITH 1
-		INCREMENT BY 1
-		NO MAXVALUE
-		CYCLE
-		CACHE 10
--- Deploy/run-updates-local.ps1
+++ Deploy/run-updates-local.ps1
@@ -1,15 +1,15 @@
 ﻿Write-Host "Restoring..."
 .\restore-previous.ps1 .\SQLEXPRESS `
 	"`"A:\Program Files\Microsoft SQL Server\MSSQL10_50.SQLEXPRESS\MSSQL\DATA\`"" `
-	"`"A:\Projects\Alicargo_2_3.bak`"" `
-	"`"A:\Projects\Alicargo_Files_2_3.bak`"" `
+	"`"A:\Projects\Alicargo_2_4.bak`"" `
+	"`"A:\Projects\Alicargo_Files_2_4.bak`"" `
 	"Alicargo" `
 	"Alicargo_Files" `
-	2_3 `
-	2_3
+	2_4 `
+	2_4
 
 Write-Host "Updating..."
-.\run-updates.ps1 .\SQLEXPRESS Alicargo Alicargo_Files 2_3
+.\run-updates.ps1 .\SQLEXPRESS Alicargo Alicargo_Files 2_4
 
 Write-Host "Done."
 cmd /c pause | out-null
