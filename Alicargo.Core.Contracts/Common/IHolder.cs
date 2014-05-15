996e57daf76250ce284e9a6b4bf5cd0b0b8868dc
6alyamov <6alyamov@gmail.com>
2014-05-15 23:45:00 +0400

    added Holder

---
+++ Alicargo.Core.Contracts/Common/IHolder.cs
@@ -0,0 +1,8 @@
+﻿namespace Alicargo.Core.Contracts.Common
+{
+	public interface IHolder<T>
+	{
+		T Get();
+		void Set(T value);
+	}
+}
---
+++ Alicargo.Core/Common/Holder.cs
@@ -0,0 +1,24 @@
+﻿using Alicargo.Core.Contracts.Common;
+
+namespace Alicargo.Core.Common
+{
+	public sealed class Holder<T> : IHolder<T>
+	{
+		private T _value;
+
+		public Holder(T value)
+		{
+			_value = value;
+		}
+
+		public T Get()
+		{
+			return _value;
+		}
+
+		public void Set(T value)
+		{
+			_value = value;
+		}
+	}
+}
