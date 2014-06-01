e995e250c29800c4544224435498460e4233a752
6alyamov <6alyamov@gmail.com>
2014-06-01 21:47:25 +0400

    AirWaybillData

---
+++ Alicargo.DataAccess.Contracts/Contracts/Awb/AirWaybillData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alicargo.DataAccess.Contracts.Contracts.Awb
+{
+	public sealed class AirWaybillData : AirWaybillEditData
+	{
+		public long Id { get; set; }
+		public long StateId { get; set; }
+		public DateTimeOffset CreationTimestamp { get; set; }
+		public DateTimeOffset StateChangeTimestamp { get; set; }
+	}
+}
