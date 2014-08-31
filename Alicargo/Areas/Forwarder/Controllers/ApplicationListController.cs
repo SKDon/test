6006e82a65d59b2b6d5e35dd8b28beab294290ca
6alyamov <6alyamov@gmail.com>
2014-08-31 22:22:26 +0400

    cleaned ApplicationListController for forwarder

--- Alicargo/Areas/Forwarder/Controllers/ApplicationListController.cs
+++ Alicargo/Areas/Forwarder/Controllers/ApplicationListController.cs
@@ -1,67 +1,35 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Web.Mvc;
 using Alicargo.Core.Contracts.Common;
-using Alicargo.Core.Contracts.State;
-using Alicargo.DataAccess.Contracts.Contracts.Awb;
 using Alicargo.DataAccess.Contracts.Enums;
-using Alicargo.DataAccess.Contracts.Repositories.Application;
 using Alicargo.DataAccess.Contracts.Repositories.User;
 using Alicargo.MvcHelpers.Filters;
 using Alicargo.Services.Abstract;
 using Alicargo.Services.Application;
-using Alicargo.ViewModels.Application;
 
 namespace Alicargo.Areas.Forwarder.Controllers
 {
 	[Access(RoleType.Forwarder)]
 	public partial class ApplicationListController : Controller
 	{
-		private readonly IAwbRepository _awbs;
-		private readonly ICarrierRepository _carriers;
-		private readonly IClientRepository _clients;
 		private readonly IForwarderRepository _forwarders;
 		private readonly IIdentityService _identity;
 		private readonly IApplicationListPresenter _presenter;
-		private readonly ISenderRepository _senders;
-		private readonly IStateConfig _stateConfig;
 
 		public ApplicationListController(
 			IApplicationListPresenter presenter,
-			IClientRepository clients,
-			ISenderRepository senders,
-			IAwbRepository awbs,
-			ICarrierRepository carriers,
-			IStateConfig stateConfig,
 			IIdentityService identity,
 			IForwarderRepository forwarders)
 		{
 			_presenter = presenter;
-			_clients = clients;
-			_senders = senders;
-			_awbs = awbs;
-			_carriers = carriers;
-			_stateConfig = stateConfig;
 			_identity = identity;
 			_forwarders = forwarders;
 		}
 
 		public virtual ViewResult Index()
 		{
-			var clients = _clients.GetAll()
-				.OrderBy(x => x.Nic)
-				.ToDictionary(x => x.ClientId, x => x.Nic);
-
-			var awbs = GetAwbs();
-
-			var model = new ApplicationIndexModel
-			{
-				Clients = clients,
-				AirWaybills = awbs.OrderBy(x => x.Bill).ToDictionary(x => x.Id, x => x.Bill)
-			};
-
-			return View(model);
+			return View();
 		}
 
 		[HttpPost]
@@ -72,35 +40,13 @@ namespace Alicargo.Areas.Forwarder.Controllers
 
 			Debug.Assert(_identity.Id != null);
 
-			var senderId = _senders.GetByUserId(_identity.Id.Value);
-
 			var forwarderId = _forwarders.GetByUserId(_identity.Id.Value);
 
-			var client = _clients.GetByUserId(_identity.Id.Value);
+			Debug.Assert(forwarderId != null);
 
-			var carrierId = _carriers.GetByUserId(_identity.Id.Value);
-
-			var clientId = client != null
-				? client.ClientId
-				: (long?)null;
-
-			var data = _presenter.List(_identity.Language, take, skip, orders, clientId, senderId, forwarderId, carrierId);
+			var data = _presenter.List(_identity.Language, take, skip, orders, null, null, forwarderId.Value);
 
 			return Json(data);
-		}
-
-		private IEnumerable<AirWaybillData> GetAwbs()
-		{
-			var cargoIsFlewStateId = _stateConfig.CargoIsFlewStateId;
-			var awbs = _awbs.Get().Where(x => x.StateId == cargoIsFlewStateId && x.IsActive);
-
-			if(_identity.IsInRole(RoleType.Sender))
-			{
-				var id = _identity.Id;
-				awbs = awbs.Where(x => x.SenderUserId == id);
-			}
-
-			return awbs;
 		}
 	}
 }
