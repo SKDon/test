02ca8712645a6ba17fe11bd425b19d357f55eeeb
6alyamov <6alyamov@gmail.com>
2015-05-27 23:06:45 +0400

    refactored MailSender to use custom smtp settings
    Related Work Items: #406

---
+++ Alicargo.Core.BlackBox.Tests/Email/MailSenderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Alicargo.Core.Email;
+using Alicargo.DataAccess.Contracts.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alicargo.Core.BlackBox.Tests.Email
+{
+	[TestClass]
+	public class MailSenderTests
+	{
+		private MailSender _sender;
+
+		[Ignore] // local only
+		[TestMethod]
+		public void Send_Test()
+		{
+			_sender.Send(new EmailMessage(
+				"test subject" + DateTime.UtcNow,
+				"test body " + Guid.NewGuid(),
+				"test@fr.om",
+				"6alyamov@gmail.com"));
+		}
+
+		[TestInitialize]		
+		public void TestInitialize()
+		{
+			_sender = new MailSender();
+		}
+	}
+}
--- Alicargo.Core/Email/MailSender.cs
+++ Alicargo.Core/Email/MailSender.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Configuration;
 using System.Net.Mail;
 using Alicargo.Core.Contracts.Email;
 using Alicargo.DataAccess.Contracts.Contracts;
@@ -9,21 +12,37 @@ namespace Alicargo.Core.Email
 	{
 		public void Send(params EmailMessage[] messages)
 		{
-			using (var smtpClient = new SmtpClient())
+			var section = (SmtpSection)ConfigurationManager.GetSection("mailSettings/default");
+
+			using(var smtpClient = new SmtpClient(section.Network.Host, section.Network.Port)
 			{
-				foreach (var message in messages)
+				Credentials = new NetworkCredential(
+					section.Network.UserName, section.Network.Password, section.Network.ClientDomain),
+				EnableSsl = section.Network.EnableSsl,
+				UseDefaultCredentials = section.Network.DefaultCredentials,
+				DeliveryFormat = section.DeliveryFormat,
+				DeliveryMethod = section.DeliveryMethod,
+			})
+			{
+				if (section.Network.TargetName != null)
+					smtpClient.TargetName = section.Network.TargetName;
+
+				if (section.SpecifiedPickupDirectory != null && section.SpecifiedPickupDirectory.PickupDirectoryLocation != null)
+					smtpClient.PickupDirectoryLocation = section.SpecifiedPickupDirectory.PickupDirectoryLocation;
+
+				foreach(var message in messages)
 				{
-					using (var email = new MailMessage())
+					using(var email = new MailMessage())
 					{
-						email.From = new MailAddress(message.From);
-						foreach (var to in message.To)
+						email.From = new MailAddress(message.From, message.From);
+						foreach(var to in message.To)
 						{
 							email.To.Add(to);
 						}
 
-						if (message.CopyTo != null)
+						if(message.CopyTo != null)
 						{
-							foreach (var to in message.CopyTo)
+							foreach(var to in message.CopyTo)
 							{
 								email.CC.Add(to);
 							}
@@ -33,9 +52,9 @@ namespace Alicargo.Core.Email
 						email.Body = message.Body;
 
 						email.IsBodyHtml = message.IsBodyHtml;
-						if (message.Files != null)
+						if(message.Files != null)
 						{
-							foreach (var file in message.Files)
+							foreach(var file in message.Files)
 							{
 								var stream = new MemoryStream(file.Data);
 								email.Attachments.Add(new Attachment(stream, file.Name));
