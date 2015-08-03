771d1ee26631c3c0e42425beacf6dddbdc6ef434
6alyamov <6alyamov@gmail.com>
2015-08-03 07:47:12 +0400

    added deploy script with MRN and CountInInvoce fields

---
+++ Alicargo.Deploy/Databases/Alicargo-3_3-3_4.sql
@@ -0,0 +1,31 @@
+﻿GO
+PRINT N'Altering [dbo].[Application]...';
+
+
+GO
+ALTER TABLE [dbo].[Application]
+    ADD [MRN]           NVARCHAR (MAX) NULL,
+        [CountInInvoce] INT            NULL;
+
+
+GO
+PRINT N'Refreshing [dbo].[Client_DeleteForce]...';
+
+
+GO
+EXECUTE sp_refreshsqlmodule N'[dbo].[Client_DeleteForce]';
+
+
+GO
+PRINT N'Refreshing [dbo].[Transit_GetByApplication]...';
+
+
+GO
+EXECUTE sp_refreshsqlmodule N'[dbo].[Transit_GetByApplication]';
+
+
+GO
+PRINT N'Update complete.';
+
+
+GO
