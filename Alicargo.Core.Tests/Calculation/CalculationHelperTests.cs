95a94ea4f94ada86b61b5d229fc4d34597b20bdd
6alyamov <6alyamov@gmail.com>
2015-08-11 22:00:13 +0400

    added CalculationHelperTests

---
+++ Alicargo.Core.Tests/Calculation/CalculationHelperTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Alicargo.Core.Calculation;
+using Alicargo.DataAccess.Contracts.Contracts.Application;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+
+namespace Alicargo.Core.Tests.Calculation
+{
+	[TestClass]
+	public class CalculationHelperTests
+	{
+		private Fixture _fixture;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_fixture = new Fixture();
+		}
+
+		[TestMethod]
+		public void GetProfit_ScotchCostEdited_Null_Test()
+		{
+			var data = _fixture.Create<ApplicationEditData>();
+			data.CalculationProfit = null;
+			data.CalculationTotalTariffCost = 1;
+			data.TariffPerKg = 2;
+			data.Weight = 3;
+			data.ScotchCostEdited = null;
+			data.SenderId = 1;
+			data.Count = 4;
+			data.Value = 5;
+			data.InsuranceRate = 0.6f;
+			data.FactureCostExEdited = 7;
+			data.FactureCostEdited = 8;
+			data.PickupCostEdited = 9;
+			data.TransitCostEdited = 12;
+			var tariffs = new Dictionary<long, decimal>
+			{
+				{ 1, 11 }
+			};
+
+			var actual = CalculationHelper.GetProfit(data, tariffs);
+
+			actual.ShouldBeEquivalentTo(84);
+		}
+
+		[TestMethod]
+		public void GetProfit_ScotchCostEdited_NotNull_Test()
+		{
+			var data = _fixture.Create<ApplicationEditData>();
+			data.CalculationProfit = null;
+			data.CalculationTotalTariffCost = 1;
+			data.TariffPerKg = 2;
+			data.Weight = 3;
+			data.Value = 5;
+			data.InsuranceRate = 0.6f;
+			data.FactureCostExEdited = 7;
+			data.FactureCostEdited = 8;
+			data.PickupCostEdited = 9;
+			data.TransitCostEdited = 10;
+			data.ScotchCostEdited = 11;
+
+			var actual = CalculationHelper.GetProfit(data, null);
+
+			actual.ShouldBeEquivalentTo(49);
+		}
+	}
+}
