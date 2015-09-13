caf23aacd0fd97f88de8b07f683c7dd7d42e15d7
s. galyamov <6alyamov@gmail.com>
2015-09-13 23:23:32 +0300

    added delete-old-emails script

---
+++ Alicargo.Deploy/Scripts/delete-old-emails.ps1
@@ -0,0 +1,6 @@
+﻿param (
+	[string] $server = (Read-Host -Prompt "Input server name"),
+	[string] $dbName = (Read-Host -Prompt "Input name of main database")
+)
+
+Sqlcmd -S $server -i ".\Scripts\delete-old-emails.sql" -v MainDbName = "$dbName"
